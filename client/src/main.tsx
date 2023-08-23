e21fee15de66214692be905ee1f111d6eaa686d4
Nick Spicer <nick.spicer@land.tech>
2023-08-23 09:08:10 +0100

    initial

---
+++ client/src/main.tsx
@@ -0,0 +1,41 @@
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+
+type Talk = {
+  id: number;
+  title: string;
+  description: string;
+};
+
+export const Main = () => {
+  const [talks, setTalks] = useState<Talk[]>([]);
+
+  useEffect(() => {
+    const main = async () => {
+      const result = await axios.get(`${process.env.API_URL}/talks`);
+      setTalks(result.data);
+    };
+    main();
+  }, []);
+
+  return (
+    <div className="flex flex-col h-screen">
+      <div className="bg-yellow-500 p-2 text-slate-50 font-semibold text-2xl">
+        Southwest JS
+      </div>
+      <div className="p-2 bg-slate-200 flex-grow">
+        <div className="flex space-x-2">
+          {talks.map((x) => (
+            <div key={x.id} className="rounded bg-slate-50 p-2">
+              <div className="font-semibold text-xl">{x.title}</div>
+              <div>{x.description}</div>
+              <div className="italic text-slate-500">
+                {new Date().toLocaleDateString("en-GB")}
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
---
+++ server/index.ts
@@ -0,0 +1,24 @@
+import { PrismaClient } from "@prisma/client";
+import cors from "cors";
+import express from "express";
+
+process.on("SIGTERM", process.exit);
+process.on("SIGINT", process.exit);
+
+const app = express();
+app.use(cors());
+
+const port = 8000;
+
+const prisma = new PrismaClient();
+
+app.get("/api/talks", async (req, res) => {
+  const result = await prisma.talk.findMany();
+  res.send(result);
+});
+
+app.options("/healthcheck", (req, res) => res.send());
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
