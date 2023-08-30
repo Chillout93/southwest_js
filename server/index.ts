98a9c9c65c03c6d7f0eb783515f91d80963e9317
Nick Spicer <nick.spicer@land.tech>
2023-08-30 14:19:11 +0100

    retry setup

--- server/index.ts
+++ server/index.ts
@@ -10,7 +10,7 @@ const port = 8000;
 
 const prisma = new PrismaClient();
 
-console.log("TEST DEPLOY 12345");
+console.log("TEST DEPLOY 123");
 app.post("/api/talks", async (req, res) => {
   const data = req.body as Talk;
   const talk = await prisma.talk.create({
