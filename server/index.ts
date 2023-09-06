10afc0a6d3439a993b8631de8ea4e63d59dfaa0e
Nick Spicer <nick.spicer@land.tech>
2023-09-06 20:45:47 +0100

    test commit

--- server/index.ts
+++ server/index.ts
@@ -7,10 +7,8 @@ app.use(cors());
 app.use(json());
 
 const port = 8000;
-
 const prisma = new PrismaClient();
 
-console.log("TEST DEPLOY 12345");
 app.post("/api/talks", async (req, res) => {
   const data = req.body as Talk;
   const talk = await prisma.talk.create({
